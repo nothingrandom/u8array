723d91dcc4d2a5f8f181c9bebfa0a3aa6b8bdb1c
Benjamin Hollway <benjamin@nothingrandom.com>
2020-01-20 17:42:18 +0100

    Fix for tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import u8 from '.';
 
 test('u8', (t) => {
-  const convertedArray = Array.from(u8('test'))
+  const convertedArray = [...u8('test')];
   const expected = [116, 101, 115, 116];
   t.deepEqual(convertedArray, expected);
 });
